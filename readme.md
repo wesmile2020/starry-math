856f6b48888c635bbdc146a30c21d923644fc805
lilei <1211921908@qq.com>
2025-12-03 14:47:38 +0800

    update: improve work frame

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Starry Math
+a math library for webgl/webgpu.
+
+## Installation
+
+```bash
+npm install starry-math
+```
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,42 @@
+import { type ViteUserConfig } from 'vitest/config';
+declare module "vite" {
+	interface UserConfig {
+    /**
+		* Options for Vitest
+		*/
+		test?: ViteUserConfig['test'];
+  }
+}
 import { defineConfig } from 'vite';
 import path from 'path';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src', 'index.ts'),
+      entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'StarryMath',
       fileName: 'starry-math',
     },
   },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+    },
+  },
+  plugins: [
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx,js,jsx}"',
+      }
+    }),
+  ],
 });
