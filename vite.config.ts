aadb06b7de1193ce42fa258e362ac003b5189942
weixiao <1211921908@qq.com>
2025-12-03 20:42:18 +0800

    update: improve build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import checker from 'vite-plugin-checker';
+import pkg from './package.json';
+
+const dependencies = Object.keys(pkg.dependencies);
+const globals: Record<string, string> = {};
+for (let i = 0; i < dependencies.length; i += 1) {
+  globals[dependencies[i]] = dependencies[i];
+}
 
 export default defineConfig({
   build: {
@@ -8,6 +15,14 @@ export default defineConfig({
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'StarryMath',
       fileName: 'starry-math',
+    },
+    rolldownOptions: {
+      external: dependencies,
+      output: {
+        globals: {
+          ...globals,
+        },
+      }
     },
   },
   resolve: {
